ecc536fb377248c10fc7184e0087b575beac5313
Kabin Dulal <dulalkabin99@gmail.com>
2018-05-31 14:09:54 -0400

    This is a program to print two different number in variables

---
+++ src/com/company/MathTwo.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class MathTwo {
+
+    public static void main(String[] args) {
+	    // This program assigns two variable and prints
+        int myNumber= 2;
+        double myOtherNumber =1.7938;
+
+        System.out.println("The first variable is myNumber and Value= " + myNumber );
+        System.out.println("The second variable is myOtherNumber and Value= " + myOtherNumber );
+    }
+}
